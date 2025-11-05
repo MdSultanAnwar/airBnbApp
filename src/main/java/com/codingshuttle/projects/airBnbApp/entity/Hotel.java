77d7b4eb909c55fae9ef7082abe2185020fd3bdd
Md Sultan Anwar <mdsultananwar@gmail.com>
2025-11-05 18:24:45 +0530

    Initial commit

---
+++ src/main/java/com/codingshuttle/projects/airBnbApp/entity/Hotel.java
@@ -0,0 +1,45 @@
+package com.codingshuttle.projects.airBnbApp.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import java.util.List;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "hotel")
+public class Hotel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String city;
+
+    @ElementCollection
+    private List<String> amenities;
+
+    @ElementCollection
+    private List<String> photos;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @Embedded
+    private HotelContactInfo contactInfo;
+
+    @Column(nullable = false)
+    private Boolean active;
+}
